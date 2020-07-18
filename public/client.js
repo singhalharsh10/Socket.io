bf0d89ddd7956e7d7bc79938e81d07290b682221
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-07-19 04:16:20 +0530

    gamesetupdone multiuser pending

--- public/client.js
+++ public/client.js
@@ -3,3 +3,19 @@ let socket = io()
 socket.on('connect', () => {
     document.getElementById('socketId').innerText = socket.id
 })
+
+
+// docment .qeryselector- it works same as jquery
+
+function colorTheBox(color) {
+    document.querySelector('.' + color).style.backgroundColor = color
+    setTimeout(() => {
+        document.querySelector('.' + color).style.backgroundColor = null
+
+    }, 500)
+}
+
+document.getElementById('colorIt').onclick = function() {
+    const colour = document.getElementById('selectColor').value
+    colorTheBox(colour)
+}
