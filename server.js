94d1aa8d6cfc7ec258d5c0cf5919cf6151cb9826
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-07-19 02:41:11 +0530

    connection established

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use('/', express.static(__dirname + '/public'))
+
+io.on('connection', (socket) => {
+    console.log('connection', socket.id)
+})
+
+
+server.listen(4646, () => {
+    console.log('server started on http://localhost:4646')
+})
